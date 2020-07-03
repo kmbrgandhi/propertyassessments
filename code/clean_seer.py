f4edfabc5a65896e4c6df5ae237d86414a549a26
Kavish Gandhi <kmbrgandhi@Kavishs-MacBook-Pro.local>
2020-07-03 00:08:26 -0400

    Cleaning of seer data, add true gitignore.

---
+++ code/clean_seer.py
@@ -0,0 +1,14 @@
+import numpy as np
+import pandas as pd
+
+headings = ["Year", "Postal", "state", "county", "registry", "race", "hispanic", "sex", "age", "population"]
+seer = pd.read_fwf("~/Documents/Housing/Property_Assessments/propertyassessments/raw/ma.1969_2018.19ages.txt", header=None, 
+	colspecs=[(0, 4), (4, 6), (6, 8), (8, 11), (11, 13), (13, 14), (14, 15), (15, 16), (16, 18), (18, 26)], names = headings)
+
+
+seer.to_csv("~/Documents/Housing/Property_Assessments/propertyassessments/cleaned/ma.1969_2018.19ages.csv")
+
+
+
+
+
